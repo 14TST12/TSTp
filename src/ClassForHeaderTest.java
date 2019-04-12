1d071fbed320afb4caffdb289d6038e9dd5f1ec5
14TST12 <14TST12@users.noreply.github.com>
2019-04-12 18:17:37 +0300

    new nah

--- src/ClassForHeaderTest.java
+++ src/ClassForHeaderTest.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * @author Vlad Sivko
  * @since 12.04.2019
@@ -6,5 +8,8 @@ public class ClassForHeaderTest {
         public static void main(String[] args) {
             Main MainObject = new Main();
             MainObject.HWSeparateMethod();
+            Scanner Scan = new Scanner(System.in);
+            String read = Scan.next();
+            System.out.printf("Your text is %s", read);
         }
 }

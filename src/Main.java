9eea0dee977df19d8a5f7139c13fb9de79d8276a
14TST12 <14TST12@users.noreply.github.com>
2019-04-12 13:59:29 +0300

    2nd nah

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
+    public static void HWseparatemetod()
+    {
+        System.out.println("Haiyshki");
+
+    }
     public static void main(String[] args) {
-        System.out.print("AAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        HWseparatemetod();
+        System.out.println("From main class");
     }
 }
---
+++ src/classforheadertest.java
@@ -0,0 +1,10 @@
+/**
+ * @author Vlad Sivko
+ * @since 12.04.2019
+ */
+public class classforheadertest {
+    public static void main(String[] args) {
+     Main mainobject = new Main();
+     mainobject.HWseparatemetod();
+    }
+}
